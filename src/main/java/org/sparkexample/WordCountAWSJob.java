4837048ea5081d0c536fe4dbbb5de24e5fb97345
wen <wen@192.168.1.6>
2015-04-07 13:31:13 -0700

    added argument parser

--- src/main/java/org/sparkexample/WordCountAWSJob.java
+++ src/main/java/org/sparkexample/WordCountAWSJob.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.spark.Accumulator;
 import org.apache.spark.SparkConf;
@@ -25,6 +29,7 @@ import scala.Tuple2;
 public final class WordCountAWSJob {
 
     private static final String PLATFORM = "PLATFORM";
+    private static final String PLATFORM_AWS = "AWS";
     private static final String CLUSTER_MODE = "CLUSTER.MODE";
     private static final String DEPENDENCY_JARS = "DEPENDENCY.JARS";
     private static final String LOCAL = "local";
@@ -49,32 +54,51 @@ public final class WordCountAWSJob {
         // -------------------------------------------------------------------
         // Parse Job Configuration
 
-        Map<String, String> sysPropertiesMap = new HashMap<String, String>();
+        ArgumentParser parser =
+            ArgumentParsers.newArgumentParser("Infutor Cleanser Job").defaultHelp(true)
+                .description("Runs Infutor Cleanser Job");
+        parser.addArgument("-p", "--platform").type(String.class).setDefault(PLATFORM_AWS);
+        parser.addArgument("-in", "--input").type(String.class).help("list of input directories");
+        parser.addArgument("-out", "--output").type(String.class).help("list of output directories");
+        parser.addArgument("-log", "--log").type(String.class).help("list of output directories");
 
-        if (SystemParameters.getJobPlatform() == SystemParameters.Platform.AWS) {
-            sysPropertiesMap.put(PLATFORM, "AWS");
-            sysPropertiesMap.put(CLUSTER_MODE, YARN_CLUSTER);
-        } else {
-            sysPropertiesMap.put(PLATFORM, "UNIX");
-            sysPropertiesMap.put(CLUSTER_MODE, LOCAL);
-            // sysPropertiesMap.put(DEPENDENCY_JARS, "s3n://path/dependency.jar");
-        }
-
-        sysPropertiesMap.put(INPUT_DATA, args[0]);
-        sysPropertiesMap.put(OUTPUT_DATA, args[1]);
+        Namespace arguments = parser.parseArgsOrFail(args);
+        System.out.println(arguments);
+        System.out.println(arguments.get("platform"));
+        
+//        Map<String, String> sysPropertiesMap = new HashMap<String, String>();
+//
+//        if (SystemParameters.getJobPlatform() == SystemParameters.Platform.AWS) {
+//            sysPropertiesMap.put(PLATFORM, "AWS");
+//            sysPropertiesMap.put(CLUSTER_MODE, YARN_CLUSTER);
+//        } else {
+//            sysPropertiesMap.put(PLATFORM, "UNIX");
+//            sysPropertiesMap.put(CLUSTER_MODE, LOCAL);
+//            // sysPropertiesMap.put(DEPENDENCY_JARS, "s3n://path/dependency.jar");
+//        }
+//
+//        sysPropertiesMap.put(INPUT_DATA, args[0]);
+//        sysPropertiesMap.put(OUTPUT_DATA, args[1]);
 
         // --------------------------------------------------------------------
         // Set up Spark Configuration
         SparkConf sparkConf = new SparkConf().setAppName("WordCount on AWS Spark");
 
-        sparkConf.setMaster(sysPropertiesMap.get(CLUSTER_MODE));
-        if (sysPropertiesMap.get(CLUSTER_MODE).equals(LOCAL)) {
-            sparkConf.set("spark.executor.memory", "2g");
+        if (PLATFORM_AWS.equalsIgnoreCase(arguments.get("platform").toString())) {
+            sparkConf.setMaster(YARN_CLUSTER);
         } else {
-            if (sysPropertiesMap.get(DEPENDENCY_JARS) != null) {
-                sparkConf.setJars(new String[]{ sysPropertiesMap.get(DEPENDENCY_JARS), });
-            }
+            sparkConf.setMaster(LOCAL);
         }
+
+//        
+//        sparkConf.setMaster(sysPropertiesMap.get(CLUSTER_MODE));
+//        if (sysPropertiesMap.get(CLUSTER_MODE).equals(LOCAL)) {
+//            sparkConf.set("spark.executor.memory", "2g");
+//        } else {
+//            if (sysPropertiesMap.get(DEPENDENCY_JARS) != null) {
+//                sparkConf.setJars(new String[]{ sysPropertiesMap.get(DEPENDENCY_JARS), });
+//            }
+//        }
 
         sparkConf.set("spark.serializer", KryoSerializer.class.getName());
         // ---------------------------------------------------------------------
@@ -82,15 +106,15 @@ public final class WordCountAWSJob {
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
         final Accumulator<Integer> accum = ctx.accumulator(0);
 
-        if (sysPropertiesMap.get(CLUSTER_MODE).equals(YARN_CLUSTER)) {
-            if (sysPropertiesMap.get(DEPENDENCY_JARS) != null) {
-                ctx.addJar(sysPropertiesMap.get(DEPENDENCY_JARS));
-            }
-        }
+//        if (sysPropertiesMap.get(CLUSTER_MODE).equals(YARN_CLUSTER)) {
+//            if (sysPropertiesMap.get(DEPENDENCY_JARS) != null) {
+//                ctx.addJar(sysPropertiesMap.get(DEPENDENCY_JARS));
+//            }
+//        }
 
         // ---------------------------------------------------------------------
         // Set up Map Reduce flow
-        JavaRDD<String> lines = ctx.textFile(sysPropertiesMap.get(INPUT_DATA));
+        JavaRDD<String> lines = ctx.textFile(arguments.get("input").toString());
         JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
             public Iterable<String> call(String s) {
                 accum.add(1);
@@ -115,10 +139,10 @@ public final class WordCountAWSJob {
 
         if (SystemParameters.getJobPlatform() == SystemParameters.Platform.AWS) {
            // counts.coalesce(100, false).saveAsTextFile(sysPropertiesMap.get(OUTPUT_DATA), GzipCodec.class);
-            counts.saveAsTextFile(sysPropertiesMap.get(OUTPUT_DATA), GzipCodec.class);
+            counts.saveAsTextFile(arguments.get("output").toString(), GzipCodec.class);
         } else {
            // counts.coalesce(100, false).saveAsTextFile(sysPropertiesMap.get(OUTPUT_DATA));
-            counts.coalesce(100, false).saveAsTextFile(sysPropertiesMap.get(OUTPUT_DATA));
+            counts.coalesce(100, false).saveAsTextFile(arguments.get("output").toString());
         }
 
         long runTime = (System.currentTimeMillis() - startTime) / 1000;
